aeb520ad8cc2dc7ca4a38b1b92f8b26e4bf689fc
Kostis Litsios <kostislitsios@gmail.com>
2026-01-10 12:20:42 +0200

    up

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ with st.sidebar:
         "Select model",
         [
             "meta-llama/llama-3.3-70b-instruct:free",
-            "qwen/qwen-3-235b-a22b:free",
+            "qwen/qwen3-235b-a22b:free",
             "mistralai/devstral-2512:free",
         ]
     )
