8411e60bbecbe99e4cb20ac20f43e008bfa9a4ea
Kostis Litsios <kostislitsios@gmail.com>
2026-01-10 15:03:47 +0200

    uppu

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("""
         min-height: 200px;
         padding: 40px;
     }
-    .stSidebar { max-width: 150px; }
+    .stSidebar { max-width: 200px; }
     </style>
 """, unsafe_allow_html=True)
 
